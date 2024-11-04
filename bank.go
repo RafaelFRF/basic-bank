f38f449d6fc2a3932f97ff119bf6208d39759559
RafaelFRF <rafaelfiorini.rf@gmail.com>
2024-11-04 17:32:43 -0300

    Refactor: divide code on functions

--- bank.go
+++ bank.go
@@ -2,35 +2,53 @@ package main
 
 import "fmt"
 
-func main() {
-	var accountBalance float64 = 1000.0
+var accountBalance float64 = 1000.0
 
+func main() {
+
+	initialQuestions()
+
+	var choice int = getUsersChoice()
+
+	if choice == 1 {
+		fmt.Println("Your balance is: ", accountBalance)
+	} else if choice == 2 {
+		depositAmount()
+	} else if choice == 3 {
+		withdrawAmount()
+	} else {
+		fmt.Println("Goodbye!")
+	}
+}
+
+func initialQuestions() {
 	fmt.Println("Welcome to Go Bank!")
 	fmt.Println("What do you want to do?")
 	fmt.Println("1. Check balance")
 	fmt.Println("2. Deposit money")
 	fmt.Println("3. Withdraw money")
 	fmt.Println("4. Exit")
+}
 
-	var choice int
+func getUsersChoice() int {
 	fmt.Print("Your choice: ")
+	var choice int
 	fmt.Scan(&choice)
+	return choice
+}
 
-	if choice == 1 {
-		fmt.Println("Your balance is: ", accountBalance)
-	} else if choice == 2 {
-		fmt.Println("Your deposit: ")
-		var depositAmount float64
-		fmt.Scan(&depositAmount)
-		accountBalance += depositAmount
-		fmt.Println("Balance updated! New amount:", accountBalance)
-	} else if choice == 3 {
-		fmt.Println("Withdraw amount: ")
-		var withdrawAmount float64
-		fmt.Scan(&withdrawAmount)
-		accountBalance -= withdrawAmount
-		fmt.Println("Balance updated! New amount:", accountBalance)
-	} else if choice == 4 {
-	}
+func withdrawAmount() {
+	fmt.Print("Withdraw amount: ")
+	var withdrawAmount float64
+	fmt.Scan(&withdrawAmount)
+	accountBalance -= withdrawAmount
+	fmt.Println("Balance updated! New amount:", accountBalance)
+}
 
+func depositAmount() {
+	fmt.Print("Your deposit: ")
+	var depositAmount float64
+	fmt.Scan(&depositAmount)
+	accountBalance += depositAmount
+	fmt.Println("Balance updated! New amount:", accountBalance)
 }
